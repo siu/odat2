421e8f3b3d2555de355d49c564fbf72266cb17df
David Sinuela <siu.4coders@gmail.com>
2009-02-03 22:25:11 +0000

    Set Rails version to 2.1.2, updated tests to support this version
    git-svn-id: http://siu.homelinux.com/odat2/trunk@8 8f90cf6b-ca61-0410-8124-aa3ac0137aa4

--- test/functional/admin/admin_section_controller_test.rb
+++ test/functional/admin/admin_section_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Admin::AdminSectionControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
+  def test_the_truth
     assert true
   end
 end
--- test/functional/admin/regions_controller_test.rb
+++ test/functional/admin/regions_controller_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 class Admin::RegionsControllerTest < ActionController::TestCase
-  test "should get index" do
+  def test_should_get_index
     get :index
     assert_response :success
     assert_not_nil assigns(:regions)
   end
 
-  test "should get new" do
+  def test_should_get_new
     get :new
     assert_response :success
   end
 
-  test "should create region" do
+  def test_should_create_region
     assert_difference('Region.count') do
       post :create, :region => { :name => 'New Region' }
     end
@@ -20,23 +20,23 @@ class Admin::RegionsControllerTest < ActionController::TestCase
     assert_redirected_to admin_region_path(assigns(:region))
   end
 
-  test "should show region" do
+  def test_should_show_region
     get :show, :id => regions(:one).id
     assert_response :success
   end
 
-  test "should get edit" do
+  def test_should_get_edit
     get :edit, :id => regions(:one).id
     assert_response :success
   end
 
-  test "should update region" do
+  def test_should_update_region
     put :update, :id => regions(:one).id, :region => { 
 	    :name => 'Updated region' }
     assert_redirected_to admin_region_path(assigns(:region))
   end
 
-  test "should destroy region" do
+  def test_should_destroy_region
     assert_difference('Region.count', -1) do
       delete :destroy, :id => regions(:one).id
     end
