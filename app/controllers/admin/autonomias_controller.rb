c4d88a435fd741d0265016c3a7a12d6f06ee7298
David Sinuela <siu.4coders@gmail.com>
2009-02-03 22:27:45 +0000

    Removed Admin::AutonomiasController
    git-svn-id: http://siu.homelinux.com/odat2/trunk@9 8f90cf6b-ca61-0410-8124-aa3ac0137aa4

--- app/controllers/admin/autonomias_controller.rb
+++
@@ -1,2 +0,0 @@
-class Admin::AutonomiasController < ApplicationController
-end
--- test/functional/admin/autonomias_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class Admin::AutonomiasControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_the_truth
-    assert true
-  end
-end
