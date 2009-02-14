789b494cda1cc5818e2b23848fe4784253f596d4
David Sinuela <siu.4coders@gmail.com>
2009-02-14 18:04:04 +0000

    Added will_paginate to centers
    git-svn-id: http://siu.homelinux.com/odat2/trunk@11 8f90cf6b-ca61-0410-8124-aa3ac0137aa4

--- app/controllers/admin/centers_controller.rb
+++ app/controllers/admin/centers_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CentersController < Admin::AdminSectionController
-  resource_this
+  resource_this :will_paginate => true
   alias :center_url :admin_center_url
   alias :centers_url :admin_centers_url
   alias :center_path :admin_center_path
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,7 @@
 class Region < ActiveRecord::Base
   validates_presence_of :name
+
+  def self.per_page
+    5
+  end
 end
