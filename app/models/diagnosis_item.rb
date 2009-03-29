8866546be2e2e7898c8bb0d3983fa455498ec2b0
David Siñuela Pastor <siu.4coders@gmail.com>
2009-03-29 13:25:56 +0200

    Few additions to DiagnosisItem

--- app/models/diagnosis_item.rb
+++ app/models/diagnosis_item.rb
@@ -6,7 +6,25 @@ class DiagnosisItem < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :description
 
+  def self.root
+    self.find(1)
+  end
+
   def self.categories
     self.find(1).direct_children
   end
+
+  def self.subcategories
+    self.categories.collect{ |c| c.direct_children }.flatten!
+  end
+
+  def subcategories
+    self.direct_children
+  end
+
+  def is_parent?(item)
+    (item[left_col_name] > self[left_col_name]) and 
+      (item[right_col_name] < self[right_col_name])
+  end
+
 end
--- test/unit/diagnosis_item_test.rb
+++ test/unit/diagnosis_item_test.rb
@@ -25,6 +25,17 @@ class DiagnosisItemTest < ActiveSupport::TestCase
     assert_equal "<p><strong>tested</strong></p>", diagnosis.description_html
   end
 
+  def test_should_create_test_tree
+    create_test_tree
+    assert_equal 9, DiagnosisItem.find(:all).size
+  end
+
+  def test_root_is_root
+    create_test_tree
+    d = DiagnosisItem.root
+    assert d.root?
+  end
+
   def test_should_return_categories
     create_test_tree
     diagnoses = DiagnosisItem.categories
@@ -32,62 +43,79 @@ class DiagnosisItemTest < ActiveSupport::TestCase
     assert_equal 3, diagnoses.size
   end
 
-  def test_should_create_test_tree
+  def test_should_return_subcategories
     create_test_tree
-    assert_equal 9, DiagnosisItem.find(:all).size
+    diagnoses = DiagnosisItem.subcategories
+    assert !diagnoses.nil?
+    assert_equal 5, diagnoses.size
   end
 
-  def test_first_should_be_a_root
+  def test_category_should_return_subcategories
     create_test_tree
-    d = DiagnosisItem.find(1)
-    assert d.root?
+    category = DiagnosisItem.categories.first
+    assert !category.nil?
+    assert_equal 3, category.subcategories.size
+  end
+
+  def test_subcategory_is_children_of_category_and_root
+    create_test_tree
+    assert @c1.is_parent?(@s1_1)
+    assert @r.is_parent?(@s1_1)
+
+    assert !@c1.is_parent?(@s2_1)
+    assert @r.is_parent?(@s2_1)
   end
 
 protected
   def create_test_tree
     # Create the root node
-    r = DiagnosisItem.create(
-	  :id => 1, # This makes no effect, but it assigns 1 by default
+    @r = DiagnosisItem.create(
+	  :id => 1, # This makes no effect, but it assigns 1 to root by default
 	  :name => 'Root', 
 	  :description => 'Root node')
 
     # Create the 3 main categories
-    c1 = DiagnosisItem.create(
+    @c1 = DiagnosisItem.create(
 	  :name => 'Cat1', 
 	  :description => 'Description of cat 1')
-    r.add_child(c1)
-    c2 = DiagnosisItem.create(
+    @r.add_child(@c1)
+    @c2 = DiagnosisItem.create(
 	  :name => 'Cat2', 
 	  :description => 'Description of cat 2')
-    r.add_child(c2)
-    c3 = DiagnosisItem.create(
+    @r.add_child(@c2)
+    @c3 = DiagnosisItem.create(
 	  :name => 'Cat3', 
 	  :description => 'Description of cat 3')
-    r.add_child(c3)
+    @r.add_child(@c3)
 
-    # Create 3 elements for category 1
-    e1_1 = DiagnosisItem.create(
-	  :name => 'Elem 1.1', 
-	  :description => 'Description of element 1.1')
-    c1.add_child(e1_1)
-    e1_2 = DiagnosisItem.create(
-	  :name => 'Elem 1.2', 
-	  :description => 'Description of element 1.2')
-    c1.add_child(e1_2)
-    e1_3 = DiagnosisItem.create(
-	  :name => 'Elem 1.3', 
-	  :description => 'Description of element 1.3')
-    c1.add_child(e1_3)
+    # Create 3 subcategories for category 1
+    @s1_1 = DiagnosisItem.create(
+	  :name => 'Subcat 1.1', 
+	  :description => 'Description of subcategory 1.1')
+    @c1.add_child(@s1_1)
+    @s1_2 = DiagnosisItem.create(
+	  :name => 'Subcat 1.2', 
+	  :description => 'Description of subcategory 1.2')
+    @c1.add_child(@s1_2)
+    @s1_3 = DiagnosisItem.create(
+	  :name => 'Subcat 1.3', 
+	  :description => 'Description of subcategory 1.3')
+    @c1.add_child(@s1_3)
 
-    # Create 2 elements for category 2
-    e2_1 = DiagnosisItem.create(
-	  :name => 'Elem 2.1', 
-	  :description => 'Description of element 2.1')
-    c2.add_child(e2_1)
-    e2_2 = DiagnosisItem.create(
-	  :name => 'Elem 2.2', 
-	  :description => 'Description of element 2.2')
-    c2.add_child(e2_2)
+    # Create 2 subcategories for category 2
+    @s2_1 = DiagnosisItem.create(
+	  :name => 'Subcat 2.1', 
+	  :description => 'Description of subcategory 2.1')
+    @c2.add_child(@s2_1)
+    @s2_2 = DiagnosisItem.create(
+	  :name => 'Subcat 2.2', 
+	  :description => 'Description of subcategory 2.2')
+    @c2.add_child(@s2_2)
+
+    # It is needed to reload all objects to update the internal variables
+    [@r, @c1, @c2, @c3, @s1_1, @s1_2, @s1_3, @s2_1, @s2_2].each do |i| 
+	i.reload
+    end
   end
 
 end
