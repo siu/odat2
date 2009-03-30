131a21b7cc105bb1cec84ef99b1247a71732ed9a
David Siñuela Pastor <siu.4coders@gmail.com>
2009-03-30 17:17:15 +0200

    Removed resource this plugin

--- app/controllers/admin/centers_controller.rb
+++ app/controllers/admin/centers_controller.rb
@@ -1,7 +1,2 @@
 class Admin::CentersController < Admin::AdminSectionController
-  resource_this :will_paginate => true
-  alias :center_url :admin_center_url
-  alias :centers_url :admin_centers_url
-  alias :center_path :admin_center_path
-  alias :centers_path :admin_centers_path
 end
--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -1,3 +1,2 @@
 class MedicalRecordsController < ApplicationController
-  resource_this :will_paginate => true
 end
