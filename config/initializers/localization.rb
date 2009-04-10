ae35db3bbd5eeeea62c9e421505bbbe5288ef7c4
David Siñuela Pastor <siu.4coders@gmail.com>
2009-04-10 18:34:37 +0200

    Creation/Edition/Suspension/Unsuspension of users in admin section. Fixed localization.

---
+++ config/initializers/localization.rb
@@ -0,0 +1,8 @@
+# in config/initializer/locale.rb
+
+# tell the I18n library where to find your translations
+# I18n.load_path += Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ]
+
+# you can omit this if you're happy with English as a default locale
+# I18n.default_locale = "es"
+
