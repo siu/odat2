c7be2463afc63a6a0cc159f239ba52f3184d54cf
David Siñuela Pastor <siu.4coders@gmail.com>
2009-04-21 18:51:13 +0200

    Fixed test for show action in pages controller.

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
   test "should show page" do
+    login_as_user
     get :show, :permalink => pages(:docs).permalink
     assert_response :success
   end
