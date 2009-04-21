d155973e8c5a0bd8cc7d897713572e885bf70396
David Siñuela Pastor <siu.4coders@gmail.com>
2009-04-21 20:39:06 +0200

    Validation of Report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,4 @@
 class Report < ActiveRecord::Base
+  validates_presence_of :signature
+  validates_presence_of :signed_on
 end
--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  test "create valid report" do
+    report = create_report
+
+    assert report.valid?
+  end
+
+  test "should require a signature" do
+    report = create_report(:signature => nil)
+
+    assert !report.valid?
+    assert report.errors.on(:signature)
+  end
+
+  test "should require a signed_on date" do
+    report = create_report(:signed_on => nil)
+
+    assert !report.valid?
+    assert report.errors.on(:signed_on)
+  end
+
+protected
+  def create_report(options = {})
+    report = Report.create({
+      :signature => 'John Smith',
+      :signed_on => Time.now }.merge(options))
+    report
   end
 end
