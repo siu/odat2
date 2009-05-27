3cf0307df87bde2aaa7dec0346d3474eb7608973
David Siñuela Pastor <siu.4coders@gmail.com>
2009-05-27 11:04:58 +0200

    Completely removed shoulda ;)

--- test/functional/admin/dashboard_controller_test.rb
+++ test/functional/admin/dashboard_controller_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 class Admin::DashboardControllerTest < ActionController::TestCase
-  should "reply not found if not logged in" do
+  test "shoul reply not found if not logged in" do
     get :index
     assert_response :not_found
   end
 
-  should "reply not found if logged in as user" do
+  test "shoul reply not found if logged in as user" do
     login_as_user
     get :index
     assert_response :not_found
   end
 
-  should "render index if logged in as admin" do
+  test "shoul render index if logged in as admin" do
     login_as_admin
     get :index
     assert_response :success
