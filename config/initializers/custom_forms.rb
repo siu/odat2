13333848d26cd814aa004876079999eedddb7895
David Siñuela Pastor <siu.4coders@gmail.com>
2009-06-15 18:32:04 +0200

    Unified form buttons

--- config/initializers/custom_forms.rb
+++ config/initializers/custom_forms.rb
@@ -13,3 +13,18 @@ module ActionView
   end
 end
 
+module Odat
+  class FormBuilder < ActionView::Helpers::FormBuilder
+    def submit_or_cancel(submit_label,
+                          cancel_label,
+                          cancel_url, 
+                          submit_opts = {},
+                          cancel_opts = {})
+      text = submit(submit_label, submit_opts) 
+      text << _(' o ') << @template.link_to(cancel_label, cancel_url, cancel_opts)
+      text
+    end
+  end
+end
+
+ActionView::Base.default_form_builder = Odat::FormBuilder
