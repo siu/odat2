487408895a5bccd7e965a464b8d1f8f52afad5f3
David Siñuela Pastor <siu.4coders@gmail.com>
2009-07-14 17:26:20 +0200

    Form for report field associations. Facebox for popups.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+jQuery(document).ready(function($) {
+  $('#login').focus();
+  $('a[rel*=facebox]').facebox()
+}) 
+//
 function update_main_diagnosis_select(obj, id, name) {
   if(obj.checked) {
     $('#odat_diagnosis_main_diagnosis_item_id').append('<option value="' + id + '">' +name+ '</option>');
