8e3cccaa6285887f2c38675b33f40a60cf7fbd96
David Siñuela Pastor <siu.4coders@gmail.com>
2009-07-14 12:07:23 +0200

    Finally got comparative report templates and report field template associations working

--- app/models/comparative_report_template.rb
+++ app/models/comparative_report_template.rb
@@ -1,7 +1,7 @@
 class ComparativeReportTemplate < ActiveRecord::Base
   has_many :comparative_report_template_field_assignments
   has_many :report_field_templates, 
-    :through => :comparative_report_template_field_assignments
+    :through => :comparative_report_template_field_assignments, :autosave => true, :uniq => true
 
   accepts_nested_attributes_for :comparative_report_template_field_assignments, :allow_destroy => true
   has_many :comparative_reports
@@ -15,24 +15,4 @@ class ComparativeReportTemplate < ActiveRecord::Base
     o
   end
 
-  def report_field_template_ids=(ids)
-    self.write_attribute(:report_field_template_ids, ids)
-    self.order_report_fields(ids)
-  end
-
-  # what about adding methods to the has_many declaration?
-  # could work, sample:
-  # http://blog.katipo.co.nz/?p=25
-  # http://refactormycode.com/codes/357-recreating-a-list-with-acts_as_list
-  
-protected
-
-  def order_report_fields(ordered_ids)
-    ordered_ids.each_with_index do |id, index|
-      begin
-        self.comparative_report_template_field_assignments.find_by_report_field_template_id(id).position = index
-      rescue
-      end
-    end
-  end
 end
