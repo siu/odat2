346484e856ce2d07e567b570457c1cf7b4778bc1
David Siñuela Pastor <siu.4coders@gmail.com>
2009-11-06 02:34:59 +0100

    Changes in main menu, now it opens on hover

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,21 +21,21 @@ jQuery(document).ready(function($) {
 
   $("ul.subnav").parent().append("<span></span>");
 
-  $("li.navcat span").click(function() { //When trigger is clicked...
+  $("li.navcat span, li.navcat").hover(function() { //When hover the menu
 
     //Following events are applied to the subnav itself (moving subnav up and down)
-    $(this).parent().find("ul.subnav").show(); //Drop down the subnav on click
+    $(this).parent().find("ul.subnav").show(); //Drop down the subnav
 
     $(this).parent().hover(function() {
       }, function(){
-      $(this).parent().find("ul.subnav").hide(); //When the mouse hovers out of the subnav, move it back up
+      $(this).parent().find("ul.subnav").hide(); //When the mouse hovers out of the subnav, hide it again
       });
 
     //Following events are applied to the trigger (Hover events for the trigger)
     }).hover(function() {
-      $(this).addClass("subhover"); //On hover over, add class "subhover"
+      $(this).addClass("subhover"); //On hover over
       }, function(){	//On Hover Out
-      $(this).removeClass("subhover"); //On hover out, remove class "subhover"
+      $(this).removeClass("subhover");
       });
 }) 
 //
