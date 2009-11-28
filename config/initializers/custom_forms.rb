c17fd017092300dea9e263a506cf391c49440c09
David Siñuela Pastor <siu.4coders@gmail.com>
2009-11-28 13:28:21 +0100

    Localized custom forms

--- config/initializers/custom_forms.rb
+++ config/initializers/custom_forms.rb
@@ -4,7 +4,7 @@ module ActionView
     module FormHelper
       def label_with_require(object_name, method, text = nil, options = {})
         text = (text.blank? ? nil : text.to_s) || method.to_s.humanize
-        text << '<strong><sup>*</sup></strong>' if options[:required] 
+        text += '<strong><sup>*</sup></strong>' if options[:required] 
         text = %Q{<span class="requiredField">#{text}</span>} if options[:required]
         options.delete(:required)
         @template.label_without_require(object_name, method, text, options)
@@ -22,7 +22,7 @@ module Odat
                           submit_opts = {},
                           cancel_opts = {})
       text = submit(submit_label, submit_opts) 
-      text << _(' o ') << @template.link_to(cancel_label, cancel_url, cancel_opts)
+      text << N_(' o ') << @template.link_to(cancel_label, cancel_url, cancel_opts)
       text
     end
   end
