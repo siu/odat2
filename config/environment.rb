5b2e8576ca91245001d39d6519424d90c4e76c16
David Siñuela Pastor <siu.4coders@gmail.com>
2009-11-30 20:11:54 +0100

    Move secrets to config/initializers/odat.rb

--- config/environment.rb
+++ config/environment.rb
@@ -69,23 +69,9 @@ Rails::Initializer.run do |config|
 
   # Localization can be found in config/initializers/localization.rb
   config.i18n.default_locale = :es
-  AVAILABLE_LANGUAGES = { 'Español' => 'http://localhost:3000/es', 
-                          'Português' => 'http://localhost:3000/pt', 
-                          'العربية' => 'http://localhost:3000/ar'}
 
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
-  # no regular words or you'll be exposed to dictionary attacks.
-  config.action_controller.session = {
-    :session_key => '_odat2_session',
-    :secret      => 'b0871da6e58c06d06e313751e23f684fb7924cc0aa512504b07c06ef0cd9fa026ef5277511223ff2b62d20c24310ea6f0e80cb6588a2a81460b05875ea59def6'
-  }
+  # The edition/release configuration of the app is located in config/initializers/odat.rb
 
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with "rake db:sessions:create")
-  # config.action_controller.session_store = :active_record_store
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
--- config/initializers/odat.rb
+++ config/initializers/odat.rb
@@ -1,3 +1,30 @@
 require 'action_controller/request'
+
+ODAT_HOST = 'http://localhost:3000'
+#ActionController::Base.relative_url_root = '/es'
+SESSION_SECRET = 'set_this_Secret-fef90b74968219df44040ebce2a0f37e'
+
+# Restful authentication
+REST_AUTH_SITE_KEY         = 'change_this_secret-asfdsakjdfhksaudhiu293urg293rgfbds'
+REST_AUTH_DIGEST_STRETCHES = 10
+
+AVAILABLE_LANGUAGES = { 'Español' => "#{ODAT_HOST}/es", 
+                        'Português' => "#{ODAT_HOST}/pt", 
+                        'العربية' => "#{ODAT_HOST}/ar"}
+
 I18n.default_locale = :es
-ActionController::Base.relative_url_root = '/es'
+I18n.locale = :es
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+#ActionController::Base.session_store = :active_record_store
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {  
+  :key => "_odat_#{I18n.locale.language}_session",
+  :secret => SESSION_SECRET
+}
