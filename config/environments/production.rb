22bbd19b353650133de909ba2613aaaba7430352
David Siñuela Pastor <siu.4coders@gmail.com>
2009-11-30 20:12:50 +0100

    Default to sqlite databases and add a comment in production environment configuration file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ config.action_controller.perform_caching             = true
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
 
+#ActionController::Base.session_store = :active_record_store
+
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
