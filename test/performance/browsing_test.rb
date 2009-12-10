25a0d0016caa063d3a21d3abbecf6bc2a9ce695f
David Siñuela Pastor <siu.4coders@gmail.com>
2009-12-10 20:24:59 +0100

    Added some performance benchmark tests

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -6,4 +6,25 @@ class BrowsingTest < ActionController::PerformanceTest
   def test_homepage
     get '/'
   end
+
+  def test_dashboard
+    login_as_user
+    get '/'
+  end
+
+  def test_medical_records_index
+    login_as_user
+    get '/expedientes'
+  end
+
+  def test_center_reports_index
+    login_as_user
+    get '/center_reports'
+  end
+
+protected
+
+  def login_as_user
+    post '/session/create', :login => 'quentin', :password => 'monkey'
+  end
 end
