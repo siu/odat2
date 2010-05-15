12348b0f94df9042bee4ca7b9fa40d46e686b391
David Siñuela Pastor <siu.4coders@gmail.com>
2010-05-16 01:37:57 +0200

    Installation patch for authentication is not needed

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-patch vendor/plugins/restful_authentication/lib/authorization/stateful_roles.rb install/patches/no_activation.diff
 patch vendor/plugins/prawnto/lib/prawnto/template_handler/compile_support.rb install/patches/prawnto-fix_https.diff
