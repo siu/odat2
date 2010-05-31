05791c977aa60db85f60326f0bb8dd1a27b32089
David Siñuela Pastor <siu.4coders@gmail.com>
2010-05-31 16:19:23 +0200

    Stick to rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 if RUBY_VERSION >= '1.9'
   Encoding.default_internal = 'utf-8'
