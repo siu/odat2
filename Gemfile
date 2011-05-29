24681e42281573e2a1c3cff4635b17912cea1874
David Siñuela Pastor <siu.4coders@gmail.com>
2011-05-29 17:59:11 +0200

    Use bundler to manage gems

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source :gemcutter
+
+gem "rails", "2.3.11"
+gem "sqlite3"
+
+gem 'authlogic'
+gem 'will_paginate', '~> 2.3.11'
+gem 'mocha'
+
+gem 'RedCloth', '4.2.3'
+gem 'comma'
+
+gem 'gettext', '>=1.9.3', :require => false, :group => :development
+gem 'fast_gettext', '>=0.5'
+gem 'gettext_i18n_rails'
+
+gem 'prawn', '0.6.3'
+gem 'prawn-format', :require => 'prawn/format'
+
+gem 'SyslogLogger', :require => 'syslog_logger'
+
--- config/environment.rb
+++ config/environment.rb
@@ -22,29 +22,6 @@ Rails::Initializer.run do |config|
   # PDF reports load path
   config.autoload_paths << "#{RAILS_ROOT}/app/reports"
   
-  config.gem 'qoobaa-sqlite3-ruby',  :lib => 'sqlite3',  
-                                     :source => 'http://gems.github.com'  
-  config.gem "mocha"
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-  config.gem 'RedCloth'
-
-  config.gem "gettext", :version => '>=1.9.3', :lib => false
-  config.gem "fast_gettext", :version => '>=0.4.8'
-  config.gem "gettext_i18n_rails"
-
-  config.gem "prawn",                :version => '0.6.3'
-  config.gem "prawn-format",         :lib => 'prawn/format'
-  config.gem "SyslogLogger",         :lib => 'syslog_logger'
-  config.gem "authlogic"
-
-  config.gem "fastercsv", 
-    :version => '1.5.3'
-
-  config.gem "crafterm-comma",
-    :version => '0.2.2',
-    :lib => 'comma',
-    :source => 'http://gems.github.com'  
-
   config.time_zone = 'UTC'
   config.i18n.default_locale = "es"
 
