1200ded9c473f51084c9262bd1885e38424a152c
David Siñuela Pastor <siu.4coders@gmail.com>
2011-07-25 17:48:03 +0200

    diagnosis_item: Remove old validation

--- app/models/diagnosis_item.rb
+++ app/models/diagnosis_item.rb
@@ -4,16 +4,9 @@ class DiagnosisItem < ActiveRecord::Base
   acts_as_html_text
 
   validates_presence_of :name
-  #validate :has_description_if_leaf
 
   def is_ancestor_of_any?(items)
     items.any? { |i| i.is_or_is_descendant_of?(self) }
   end
 
-protected
-  def has_description_if_leaf
-    if !self.new_record? and self.level >= 2 and self.description.blank?
-      errors.add_to_base(_('Descripción no puede estar vacía'))
-    end
-  end
 end
