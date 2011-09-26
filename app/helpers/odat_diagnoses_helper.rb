37db91acb63215c87696cce29c262ff34f5e567f
David Siñuela Pastor <siu.4coders@gmail.com>
2011-09-26 23:17:01 +0200

    Smarter main_diagnosis dropdown addition, support for odat_diagnosis_items fourth level

--- app/helpers/odat_diagnoses_helper.rb
+++ app/helpers/odat_diagnoses_helper.rb
@@ -6,8 +6,31 @@ module OdatDiagnosesHelper
       item.id,
       diagnoses.include?(item),
       :id => "diagnosis_item_ids_#{item.id}",
-      :onchange => "update_main_diagnosis_select(this, #{item.id}, \"#{h item.name}\")")
+      :class => "leaf")
     output << label_tag("diagnosis_item_ids[#{item.id}]", item.name)
+    if not item.leaf?
+      output << display_checkboxes_for_diagnosis(f, item, diagnoses)
+    end
+    output
+  end
+
+  def display_dropdown_for_diagnosis(f, item, diagnoses)
+    output = ' '
+    output << f.collection_select(:diagnosis_item_ids, item.children, :id, :name)
+  end
+
+  def display_checkboxes_for_diagnosis(f, item, diagnoses)
+    output = '<ul>'
+    item.children.each do |item|
+      output << '<li>' << radio_button_tag("odat_diagnosis[diagnosis_item_ids][]", 
+        item.id,
+        diagnoses.include?(item),
+        :id => "diagnosis_item_ids_#{item.id}")
+      output << label_tag("diagnosis_item_ids[#{item.id}]", item.name)
+      output << '</li>'
+    end
+    output << '</ul>'
+    output
   end
 
   def hide_attr_if(condition)
