ec7aa1c7fd6606fb777ac55427a356a4557dac55
David Siñuela Pastor <siu.4coders@gmail.com>
2011-09-27 23:44:16 +0200

    Proper support for different types of last level of diagnosis_items elements (for now)

--- app/helpers/odat_diagnoses_helper.rb
+++ app/helpers/odat_diagnoses_helper.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 module OdatDiagnosesHelper
-  def display_item_for_diagnosis(f, item, diagnoses)
+  def item_for_diagnosis(f, item, diagnoses)
     output = ''
     output << check_box_tag("odat_diagnosis[diagnosis_item_ids][]", 
       item.id,
@@ -9,17 +9,33 @@ module OdatDiagnosesHelper
       :class => "leaf")
     output << label_tag("diagnosis_item_ids[#{item.id}]", item.name)
     if not item.leaf?
-      output << display_checkboxes_for_diagnosis(f, item, diagnoses)
+      if item.multiple_select?
+        output << check_boxes_for_diagnosis(f, item, diagnoses)
+      else
+        output << radio_boxes_for_diagnosis(f, item, diagnoses)
+      end
     end
     output
   end
 
-  def display_dropdown_for_diagnosis(f, item, diagnoses)
+  def check_boxes_for_diagnosis(f, item, diagnoses)
     output = ' '
     output << f.collection_select(:diagnosis_item_ids, item.children, :id, :name)
+
+    output = '<ul>'
+    item.children.each do |item|
+      output << '<li>' << check_box_tag("odat_diagnosis[diagnosis_item_ids][]", 
+        item.id,
+        diagnoses.include?(item),
+        :id => "diagnosis_item_ids_#{item.id}")
+      output << label_tag("diagnosis_item_ids[#{item.id}]", item.name)
+      output << '</li>'
+    end
+    output << '</ul>'
+    output
   end
 
-  def display_checkboxes_for_diagnosis(f, item, diagnoses)
+  def radio_boxes_for_diagnosis(f, item, diagnoses)
     output = '<ul>'
     item.children.each do |item|
       output << '<li>' << radio_button_tag("odat_diagnosis[diagnosis_item_ids][]", 
