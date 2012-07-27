0e48190abadd90a955f135fcdfe529eb70be3663
David Siñuela Pastor <siu.4coders@gmail.com>
2012-07-27 16:17:24 +0200

    Dont't use active_resource nor action_mailer

--- config/environment.rb
+++ config/environment.rb
@@ -18,6 +18,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [ :active_resource, :action_mailer ]
   
   # PDF reports load path
   config.autoload_paths << "#{RAILS_ROOT}/app/reports"
